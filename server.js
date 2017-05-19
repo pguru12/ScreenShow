f596b6e412259666e5113d2aa7c0d88444e57fdc
pguru12 <pguru12@gmail.com>
2017-05-19 17:58:51 -0500

    update
    new

--- server.js
+++ server.js
@@ -1,41 +1,41 @@
-var express = require('express');
+var express = require("express");
 var request = require("request");
 //making calls to node js api key
 var app = express();
 var rP = require("request-promise");
 
-var apiKey = "2f4fe68403709710f406f4da7be00736"
-var workingDir = "C:/Users/cpagu/Documents/GitHub/ScreenShow/"
+var apiKey = "2f4fe68403709710f406f4da7be00736";
+var workingDir = "C:/Users/cpagu/Documents/GitHub/ScreenShow/";
 
 //this is a route
 //this gets executed when you reach "URL/"
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get("/", function (req, res){
+  res.send("Hello World!");
 });
 
-app.get('/movies',function (req, res) {
+app.get("/movies",function (req, res) {
     res.sendFile(workingDir + "movies.html");
 });
 
 
-app.get('/tvshows',function (req, res) {
+app.get("/tvshows",function (req, res) {
     res.sendFile(workingDir + "tvshow.html");
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log("Example app listening on port 3000!");
 });
 
 function getMovie (movie){
-  var options = { 
-        method: 'GET',
-        url: 'https://api.themoviedb.org/3/search/movie',
-        qs: { 
-            query: movie, language: 'en-US', api_key: apiKey 
+  var options = {
+        method: "GET",
+        url: "https://api.themoviedb.org/3/search/movie",
+        qs: {
+            query: movie, language: "en-US", api_key: apiKey
         },
-        headers: { 'content-type': 'application/json' },
+        headers: { "content-type": "application/json" },
         body: {},
-        json: true 
+        json: true
     };
     return rP(options);
     }
@@ -43,10 +43,10 @@ function getMovie (movie){
 //getMovie('storks')
 
 function getPerson (celebrity)
-{var options = { method: 'GET',
-  url: 'https://api.themoviedb.org/3/search/person',
-  qs: { query: celebrity, language: 'en-US', api_key: apiKey },
-  headers: { 'content-type': 'application/json' },
+{var options = { method: "GET",
+  url: "https://api.themoviedb.org/3/search/person",
+  qs: { query: celebrity, language: "en-US", api_key: apiKey },
+  headers: { "content-type": "application/json" },
   body: {},
   json: true };
 
@@ -57,26 +57,25 @@ request(options, function (error, response, info) {
   // returns after digging through the numbers and returns the idea of result
 });}
 
-
 function getShow(program){
-var options = { 
-  method: 'GET',
-    url: 'https://api.themoviedb.org/3/search/tv',
-    qs: { page: '1',
+var options = {
+  method: "GET",
+    url: "https://api.themoviedb.org/3/search/tv",
+    qs: { page: "1",
         query: program,
-        language: 'en-US',
-        api_key: '2f4fe68403709710f406f4da7be00736' 
+        language: "en-US",
+        api_key: "2f4fe68403709710f406f4da7be00736"
         },
-    body: '{}' 
+    body: "{}"
   };
 
 return rP(options);
 }
 
-app.get('/movies222',function (req, res) {
+app.get("/movies222",function (req, res) {
     getMovie("Storks").then(function(value){
         //console.log(value.results[0]);
-            res.send("Movie: " + value.results[0].original_title + " Summary: " + value.results[0].overview + " ID: " + value.results[0].id);   
+            res.send("Movie: " + value.results[0].original_title + " Summary: " + value.results[0].overview + " ID: " + value.results[0].id);
     }, function(err){
         console.log(err);
     })
